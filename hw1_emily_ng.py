df61f97c27bd88140189fb55bbad5c060d5534b4
emilylienhuang <eng@uccs.edu>
2023-01-28 10:37:01 -0700

    Finished hw1

--- hw1_emily_ng.py
+++ hw1_emily_ng.py
@@ -1,7 +1,7 @@
 """
 Homework 1
 Name: Emily Ng
-Date: 9/2/22
+Date: 1/26/23
 This program asks the user 3 creative security questions.
 After correct responses, it will print a bit of secret information.
 The program uses mathematical operators, truthy and falsy values,
@@ -9,30 +9,33 @@ while and for loops, and functions such as input, str, int, and randint.
 The answers for the security questions are as follows:
 1. It depends, will be defined in the function and marked with a variable identifier
     However, birth year is 1998 and age is 24. Don't call me old.
-2. Lienhua
+2. 31
 3.Jade
 """
 
 
 import random
 
+#this function validates user input, converts input to an integer, and checks to see if the answer is correct
+def user_validation_number(question, expected_num):
+    user_input_num = ''
+    while True:
+        print(question)
+        user_input_num = input()
 
-def user_validation_number(question, user_input_num, expected_num):
-    user_correct = False
-    while not user_correct:
-        if user_input_num != expected_num:
-            print('Incorrect number. Try again.')
-            print(question)
-            user_input_num = input()
-            while "." in user_input_num:
-                print('You didn\'t listen. No decimals. Try again.')
-                print(question)
-                user_input_num = input()
-            user_input_num = int(user_input_num)
+        if not user_input_num:
+            print('Nothing was entered')
+        if not user_input_num.isdecimal():
+            print("Must enter a number")
         else:
-            user_correct = True
+            user_input_num = int(user_input_num)
+            if user_input_num != expected_num:
+                print('Incorrect entry. Try again.')
+            else:
+                break
 
 
+#this function validates a string
 def user_validation_string(question, user_input, answer):
     user_correct = False
     while not user_correct:
@@ -47,7 +50,7 @@ def user_validation_string(question, user_input, answer):
             user_input = input()
             user_input = user_input.upper()
         else:
-            user_correct = True
+            break
 
 
 def main():
@@ -56,26 +59,17 @@ def main():
         if i == 0:
             # Here we are asking question 1 which will have a numeric result
             random_divisor = random.randint(1, 100)
-            question_1 = 'Enter your ((birth year * your age)/' + str(random_divisor) + ') - 2 without decimal values: '
-            print(question_1)
-            response = input()  # user response taken in a s a float for error handling
-            while "." in response:  # check if user has entered a decimal number
-                print('You don\'t listen. No decimals. Try again.')
-                question_1 = 'Enter your ((birth year * your age)/' + str(
-                    random_divisor) + ') - 2 without a remainder: '
-                print(question_1)
-                response = input()
-            response = int(response)
-            result = ((1998 * 24)//random_divisor) - 2  # answer to question_1
-            user_validation_number(question_1, response, result)  # will validate the user input
+            question_1 = 'Enter your ((birth year * your age)/' + str(random_divisor) +') - 2 rounded down to the nearest integer.'
+            result = ((1998 * 24.0) // random_divisor) - 2   # answer to question_1
+            if user_validation_number(question_1, result):
+                # will validate the user input and continue to the next iteration of the loop
+                continue
         elif i == 1:
-            # moves on to question 2 asking user to enter their middle name.
-            middle_name = 'LIENHUA'
-            question_2 = 'What is your middle name?'
-            print(question_2)
-            user_middle_name_entry = input()
-            user_middle_name_entry = user_middle_name_entry.upper()  # making sure user input is not influenced by case
-            user_validation_string(question_2, user_middle_name_entry, middle_name)
+            # moves on to question 2 asking user to enter the sum of the numbers in their birthday.
+            answer = 2 + 2 + 1 + 9 + 9 + 8 #born February, 2, 1998
+            question_2 = 'What is the sum of the numbers in your birthday (month, day, year)?'
+            if user_validation_number(question_2, answer):
+                continue  #will not exectue if there is an error in executing the function
         else:
             # moves on to question 3 asking user to enter their favorite plant
             plant_name = 'JADE'
@@ -83,8 +77,9 @@ def main():
             print(question_3)
             user_plant_entry = input()
             user_plant_entry = user_plant_entry.upper()  # making sure user input is not influenced by case
-            user_validation_string(question_3, user_plant_entry, plant_name)
-    print('Secret Message: I\'m wait listed for CS3080, but I want to be in the course. Not a good position.')
+            if user_validation_string(question_3, user_plant_entry, plant_name):
+                continue
+    print('Secret Message: I am an unfrugal former econ major. I spent $22 on a deoderant that smells like sh*t, and now I have to use a $22 sh*tty deoderant.')
 
 
 main()
